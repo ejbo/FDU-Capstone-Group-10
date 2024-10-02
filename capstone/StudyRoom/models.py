1fce83fa39f95a67a56504c8de8f27e00d04ad24
Jbo <jbo@JbodeMacBook-Pro.local>
2024-10-01 17:23:02 -0700

    db&room created&home page

--- capstone/StudyRoom/models.py
+++ capstone/StudyRoom/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Room(models.Model):
+    CATEGORY_CHOICES = {
+        'PR': "Private Room",
+        'PU': "Public Room",
+    }
+    host = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rooms')
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=2, choices=CATEGORY_CHOICES)
+    topic = models.ForeignKey('Topic', on_delete=models.CASCADE, related_name='rooms')
+    description = models.TextField()
+    members = models.ManyToManyField(User, related_name='members', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated_at', '-created_at']
+
+    def __str__(self):
+        return self.name
+    
+
+class Topic(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
--- capstone/StudyRoom/views.py
+++ capstone/StudyRoom/views.py
@@ -1,11 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.db.models import Q
+from .models import Room, Topic
+from .forms import RoomForm
 # Create your views here.
 def home(request):
 
-    context = {
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
 
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )  
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+    context = {
+        'rooms': rooms,
+        'topics': topics,
+        'room_count': room_count,
+    }            
+    return render(request, 'StudyRoom/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {
+        'room': room,
     }
 
-    return render(request, Studyroom/home.html, context)
+    return render(request, 'StudyRoom/room.html', context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'StudyRoom/room_form.html', context)
